1c10b2923a5c299d18d857f7563c5a79bf091b55
SKCwillie <robert@localhost.localdomain>
2023-11-16 09:42:49 -0500

    changing scope to mark email as read

--- confirmation_code.py
+++ confirmation_code.py
@@ -5,11 +5,10 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
-ATTEMPTS_ALLOWED = 10
+SCOPES = ["https://mail.google.com/"]
+ATTEMPTS_ALLOWED = 12
 
 
 def get_confirmation_code(email_address):
@@ -17,9 +16,41 @@ def get_confirmation_code(email_address):
     Reads emails from talentreef.qa@gmail.com
     Matches email address used to create hire
     Returns confirmation code to be used to activate account
+    Then marks email as read
     :param: email_address (str) talentreef.qa+{timestamp}@gmail.com
     :return: confirmation code (int)
   """
+    def helper(email_to_check, gmail_messages, attempts=0):
+        """
+        Helper to take messages from inbox and recursively call itself to loop over messages.
+        If the email matches the email_to_check, return confirmation code associated with that email
+        :param: email_to check: email used to create confirmation code
+        :param gmail_messages: unread messages from inbox generated main function
+        :param attempts: number of attempts so this function doesn't run forever if no code is received
+        :return:
+        """
+        return_code = None
+        for message in gmail_messages:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            email = msg['payload']['headers'][0]['value']
+            code = msg['snippet'].split()[-1][:-1]
+            if email == email_to_check:
+                return_code = code
+
+        if return_code:
+            msg = service.users().messages().modify(userId='me', id=message['id'], body={'removeLabelIds': ['UNREAD']}).execute()
+            return return_code
+        elif attempts == ATTEMPTS_ALLOWED:
+            return
+        else:
+            attempts += 1
+            print(f'Could not find code. {ATTEMPTS_ALLOWED - attempts} attempts left')
+            time.sleep(60/ATTEMPTS_ALLOWED)
+            helper(email_to_check, gmail_messages, attempts)
+
+
+
+
 
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
@@ -48,15 +79,7 @@ def get_confirmation_code(email_address):
         if not messages:
             print('No new messages.')
         else:
-            for message in messages:
-                msg = service.users().messages().get(userId='me', id=message['id']).execute()
-                email = msg['payload']['headers'][0]['value']
-                code = msg['snippet'].split()[-1][:-1]
-                if email == email_address:
-                    return code
-
-
-                # msg = service.users().messages().modify(userId='me', id=message['id'], body={'removeLabelIds': ['UNREAD']}).execute()
+            return helper(email_address, messages)
 
     except Exception as error:
         print(f'An error occurred: {error}')
